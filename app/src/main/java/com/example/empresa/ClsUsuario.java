203e8e6fdda9308d8a5be1873941a24c6af898e3
505 <505@academicos.cesde.edu.co>
2022-09-23 20:13:29 -0500

    Se agrego una nueva clase ClsUsuario

---
+++ app/src/main/java/com/example/empresa/ClsUsuario.java
@@ -0,0 +1,15 @@
+package com.example.empresa;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ClsUsuario {
+    private String usr;
+    private String nombre;
+    private String correo;
+    private String clave;
+    private String activo;
+}
